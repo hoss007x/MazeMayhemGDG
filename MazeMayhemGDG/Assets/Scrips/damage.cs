d2e001fd7ffa2cfe37bc35e1c23a2e927b559071
shawn <shawnmahan79@gmail.com>
2025-12-03 23:55:04 -0600

    i added a Damage script for our damage types

---
+++ MazeMayhemGDG/Assets/Scrips/damage.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    enum damageType { moving, stationary, DOT, homing }
+    [SerializeField] damageType type;
+    [SerializeField] Rigidbody rb;
+
+    [SerializeField] int damageAmount;
+    [SerializeField] int speed;
+    [SerializeField] float damageRate;
+    [SerializeField] int destroyTime;
+
+    bool isDamaging;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (type == damageType.moving || type == damageType.homing)
+        {
+            Destroy(gameObject, destroyTime);
+            if (type == damageType.moving)
+            {
+                rb.linearVelocity = transform.forward * speed;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (type == damageType.homing)
+        {
+            rb.linearVelocity = (gameManager.instance.player.transform.position - transform.position).normalized * speed * Time.deltaTime;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger)
+        {
+            return;
+        }
+        IDamage dmg = other.GetComponent<IDamage>();
+
+        if (dmg != null && type != damageType.DOT)
+        {
+            dmg.takeDamage(damageAmount);
+        }
+        if (type == damageType.homing || type == damageType.moving)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.isTrigger)
+        {
+            return;
+        }
+        IDamage dmg = other.GetComponent<IDamage>();
+        if (dmg != null && type == damageType.DOT && !isDamaging)
+        {
+            StartCoroutine(damageOther(dmg));
+        }
+    }
+    IEnumerator damageOther(IDamage d)
+    {
+        isDamaging = true;
+        d.takeDamage(damageAmount);
+        yield return new WaitForSeconds(damageRate);
+        isDamaging = false;
+    }
+}
