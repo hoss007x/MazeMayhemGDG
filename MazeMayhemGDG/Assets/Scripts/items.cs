d3aa7007e26e9ef0308f70570b395f03687c9ec6
Mykell Jenkins <mykelljenkinstrina@gmail.com>
2025-12-06 09:06:20 -0500

    Finish

---
+++ MazeMayhemGDG/Assets/Scripts/items.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class items : MonoBehaviour
+{
+    enum itemType { healing, faster, stronger }
+    [SerializeField] itemType type;
+
+    [SerializeField] int healAmount;
+    [SerializeField] int damageIncrease;
+    [SerializeField] int speedIncrease;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger)
+        { return; }
+
+        ITypesOfItems buff = other.GetComponent<ITypesOfItems>();
+
+        if (buff != null && type == itemType.healing)
+        {
+            buff.healing(healAmount);
+        }
+        if (buff != null && type == itemType.faster)
+        {
+            buff.faster(speedIncrease);
+        }
+        if (buff != null && type == itemType.stronger)
+        {
+            buff.stronger(damageIncrease);
+        }
+        if (buff != null)    
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
