8fb4e2c12507a46e00f2dca675fed5194c7595d1
shawn <shawnmahan79@gmail.com>
2025-12-15 23:24:43 -0600

    fixed flipPlatforms because of weird speed requirements

--- MazeMayhemGDG/Assets/Scripts/FlipPlatformX.cs
+++ MazeMayhemGDG/Assets/Scripts/FlipPlatformX.cs
@@ -3,21 +3,30 @@ using UnityEngine;
 
 public class FlipPlatformX : MonoBehaviour
 {
-    [Range(50, 1000)][SerializeField] float speed;
     [SerializeField] float resetTime;
-    [SerializeField] float degree;
+    [SerializeField] bool oneHundredEighty;
 
     Quaternion startRot;
     Quaternion flippedRot;
 
-    float time;
+    float speed;
+
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startRot = transform.rotation;
-        flippedRot = startRot * Quaternion.Euler(degree, 0, 0);
-
+        if (oneHundredEighty)
+        {
+            flippedRot = startRot * Quaternion.Euler(180, 0, 0);
+            speed =250;
+        }
+        else
+        {
+            flippedRot = startRot * Quaternion.Euler(90, 0, 0);
+            speed = 50;
+        }
         StartCoroutine(RotateRoutine());
 
     }
@@ -40,7 +49,7 @@ public class FlipPlatformX : MonoBehaviour
             yield return null;
 
         }
-        transform.rotation = target;
+        
         yield return new WaitForSeconds(resetTime);
 
     }
