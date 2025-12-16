d5b5e65bffd42de47ec6d398f98b1c4eb6977104
FlameOnBoy9090 <darkblade8921@gmail.com>
2025-12-16 13:09:20 -0500

    Added Turrent AI for A turrent Mini Boss

---
+++ MazeMayhemGDG/Assets/Scripts/TurrentAI.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class TurrentAI : MonoBehaviour
+{
+    Transform Player;
+
+    float distance;
+
+    public float Detect;
+    public float FireRate, nextFire;
+
+
+    public Transform head, barrel;
+    public GameObject Projecttile;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(Player.position, transform.position);
+        if (distance <= Detect)
+        {
+            head.LookAt(Player);
+            if (Time.time >= nextFire)
+            {
+                nextFire = Time.time + 1f / FireRate;
+                shoot();
+            }
+        }
+
+    }
+
+    void shoot()
+    {
+        GameObject projectile = Instantiate(Projecttile, barrel.position, head.rotation);
+        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+        Destroy(projectile, 10);
+        rb.AddForce(head.forward * 500f);
+    }
+
+
+
+
+
+}
