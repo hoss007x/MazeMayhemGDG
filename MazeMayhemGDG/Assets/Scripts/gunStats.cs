d60258c7d01b49fe060a8f6e1eac14283bdebfd3
Mykell Jenkins <mykelljenkinstrina@gmail.com>
2025-12-15 21:27:19 -0500

    save

--- MazeMayhemGDG/Assets/Scripts/gunStats.cs
+++ MazeMayhemGDG/Assets/Scripts/gunStats.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class gunStats : ScriptableObject
 {
     public GameObject gunModel;
-
     [Range(1, 10)][SerializeField] public int ShootDamage;
     [Range(3, 1000)][SerializeField] public int ShootDistances;
     [Range(0.1f, 3)][SerializeField] public float ShootRate;
